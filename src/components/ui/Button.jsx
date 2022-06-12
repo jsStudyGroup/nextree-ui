28115a5b0ab060a9716ffda6c28184196e29edbf
mansukong <mansukong@gmail.com>
2022-06-13 02:10:30 +0900

    update

---
+++ src/components/ui/Button.jsx
@@ -0,0 +1,45 @@
+import PropTypes from "prop-types";
+
+function buttonRipples(e) {
+  e.stopPropagation();
+  const el = e.currentTarget;
+  const ripples = document.createElement("i");
+
+  el.appendChild(ripples);
+  ripples.classList.add("ripples");
+
+  e = e.touches ? e.touches[0] : e;
+
+  const r = el.getBoundingClientRect(),
+    d = Math.sqrt(Math.pow(r.width, 2) + Math.pow(r.height, 2)) * 2;
+
+  ripples.style.cssText = `--s: 0; --o: 1;`;
+  ripples.offsetTop;
+  ripples.style.cssText = `--t: 1; --o: 0; --d: ${d}; --x:${
+    e.clientX - r.left
+  }; --y:${e.clientY - r.top};`;
+
+  setTimeout(() => {
+    ripples.remove();
+  }, 600);
+}
+
+function Button({ label, handleClick }) {
+  return (
+    <div
+      className='btn'
+      onClick={function (e) {
+        handleClick(), buttonRipples(e);
+      }}
+    >
+      {label}
+    </div>
+  );
+}
+
+Button.propTypes = {
+  label: PropTypes.string,
+  handleClick: PropTypes.func,
+};
+
+export default Button;
